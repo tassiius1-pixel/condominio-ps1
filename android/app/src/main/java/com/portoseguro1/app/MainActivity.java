9aab6fcbd3217048e8d6701f1554264326e38a58
Tássius <tassiius1@gmail.com>
2026-06-11 21:01:12 -0300

    feat: initialize Android platform with Capacitor integration

---
+++ android/app/src/main/java/com/portoseguro1/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.portoseguro1.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.portoseguro1.app',
+  appName: 'Porto Seguro 1',
+  webDir: 'dist'
+};
+
+export default config;
